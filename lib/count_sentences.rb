917ecb64f36891ec4dd1b2be242610868c03db2e
DeCode2018 <decode2018@gmail.com>
2018-09-19 13:14:25 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+   end_with?(".")
   end
 
   def question?
-
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
   def count_sentences
+      run_on = self.include?("...")
+      total = self.count(".?!")
+
+      # if run_on == true
+      #   total -= 3
+      # end
+      # refactored to:
+      # vvvvvvvvvvvvvv
+      ( run_on ?  total -= 3: total )
 
   end
 end
