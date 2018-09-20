4032c74d4453b1e2b6e204811aee57b4ed97fc0b
pasolre95kim <pasolre95kim@gmail.com>
2018-09-20 09:54:14 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    split_sentence = self.gsub("...", "").split("")
+    split_sentence.each do |character|
+      if character == "?" || character == "." || character == "!"
+        count += 1
+      end
+    end
+    count
   end
+    # self.split(/\?|\.|!/).size
+    # split_sentence = self.split(/\?|\.|!/)
+    # split_sentence.count
+    # binding.pry
+    # split_sentence = self.split
+    # no_empty_strings.count
+    #
 end
